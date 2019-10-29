7d65db5d9537003d3f6bd954b1b3f696c73ff00a
mreshtaiwi <mr.eshtaiwi@gmail.com>
2019-10-29 16:32:58 +0200

    creating the project

---
+++ js/lab2.js
@@ -0,0 +1,158 @@
+
+alert('please answer the 5 questions yes or no - you can use ( y , n) as answer Only');
+
+var q1 = prompt("my name is sohaib?").toLowerCase();
+while(q1 != 'y' && q1 != 'yes' && q1 != 'n' && q1 != 'no'){
+    q1 = prompt("my name is sohaib?").toLowerCase();
+}
+if(q1 === 'n' || q1 === 'no'){
+    console.log('corrct');
+    //alert('corrct');
+}
+else{
+    console.log('wrong');
+    //alert('wrong');
+}
+
+var q2 = prompt("my favourite color is blue?").toLowerCase();
+while(q2 != 'y' && q2 != 'yes' && q2 != 'n' && q2 != 'no'){
+    q2 = prompt("my favourite color is blue?").toLowerCase();
+}
+if(q2 === 'y' || q2 === 'yes'){
+    console.log('corrct');
+    //alert('corrct');
+}
+else{
+    console.log('wrong');
+    //alert('wrong');
+}
+
+var q3 = prompt("my favourite food is Maqlobeh?").toLowerCase();
+while(q3 != 'y' && q3 != 'yes' && q3 != 'n' && q3 != 'no'){
+    q3 = prompt("my favourite food is Maqlobeh?").toLowerCase();
+}
+if(q3 === 'y' || q3 === 'yes'){
+    console.log('corrct');
+    //alert('corrct');
+}
+else{
+    console.log('wrong');
+    //alert('wrong');
+}
+
+var q4 = prompt("Do i love Cats?").toLowerCase();
+while(q4 != 'y' && q4 != 'yes' && q4 != 'n' && q4 != 'no'){
+    q4 = prompt("Do i love Cats?").toLowerCase();
+}
+if(q4 === 'y' || q4 === 'yes'){
+    console.log('corrct');
+    //alert('corrct');
+}
+else{
+    console.log('wrong');
+    //alert('wrong');
+}
+
+var q4 = prompt("Do i live in Amman?").toLowerCase();
+while(q4 != 'y' && q4 != 'yes' && q4 != 'n' && q4 != 'no'){
+    q4 = prompt("Do i live in Amman?").toLowerCase();
+}
+if(q4 === 'y' || q4 === 'yes'){
+    console.log('corrct');
+    //alert('corrct');
+}
+else{
+    console.log('wrong');
+    //alert('wrong');
+}
+
+
+
+
+
+
+// var questions = ["my name is sohaib?","my favourite color is blue?","my favourite food is Maqlobeh?","Do i love Cats?","Do i live in Amman?"];
+// var answers = [];
+// var correct = ['n','y','y','y'];
+
+// for (var i = 0; i < questions.length; i++) {
+//     answers[i] = prompt(questions[i]).toLowerCase();
+// }
+
+// while (as) {
+    
+// }
+// if( answers[0] === 'n' || answers[0] === 'no'){
+//     alert('correct');
+// }else{
+//     alert('wrong');
+// }
+// if (answers[1] === 'y' || answers[1] === 'yes') {
+//     alert('correct');
+// } else {
+//     alert('wrong');
+// }
+// if (answers[2] === 'y' || answers[2] === 'yes') {
+//     alert('correct');
+// } else {
+//     alert('wrong');
+// }
+// if (answers[3] === 'y' || answers[3] === 'yes') {
+//     alert('correct');
+// } else {
+//     alert('wrong');
+// }
+
+
+// for (var check = 0; check < answers.length; check++) {
+//     if(answers[i] != 'y' || answers[i] != 'n' || answers[i] != 'yes' || answers[i] != 'no'){
+//         alert(answers[i]);
+//         // switch (answers[i]) {
+//         //     case 'y':
+                
+//         //         break;
+//         //     case 'yes':
+                
+//         //         break;
+//         //     case 'n':
+                
+//         //         break; 
+//         //     case 'no':
+                
+//         //         break;  
+//         //     default:
+//         //         break;
+//         // }
+//     }else{
+//         for (var i = 0; i < questions.length; i++) {
+//             answers[i] = prompt(questions[i]).toLowerCase();
+//         }
+//     }
+// }
+
+// for (var check = 0; check < answers.length; check++) {
+//     if((answers[i] === 'y' || answers[i] === 'n') && (answers[i] === 'yes' || answers[i] === 'no')){
+//         if(answers[i] === 'y' || answers[i] === 'yes' )
+//         {
+//             console.log('wrong answer');
+//         }else{
+//             console.log('correct answer');
+//         }
+//     }else{
+//         for (var i = 0; i < questions.length; i++) {
+//             answers[i] = prompt(questions[i]).toLowerCase();
+//         }
+//     }
+// }
+
+// var q2 = prompt().toLowerCase();
+// console.log(q2);
+
+// var q3 = prompt().toLowerCase();
+// console.log(q3);
+
+// var q4 = prompt().toLowerCase();
+// console.log(q4);
+
+// var q5 = prompt().toLowerCase();
+// console.log(q5);
